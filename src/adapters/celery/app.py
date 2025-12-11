658344bb14ea9587d79c25e494aba12d0299de7a
Zertonixa <olpin9070@gmail.com>
2025-12-12 00:08:05 +0300

    feat(booking): add celery

--- src/adapters/celery/app.py
+++ src/adapters/celery/app.py
@@ -1,3 +1,18 @@
-"""
-Тут следует хранить создание клиента Celery
-"""
+from celery import Celery
+
+from src.core.config.config import settings
+
+celery = Celery(
+    "booking",
+    broker=settings.redis_pubsub.dsn,
+    backend=settings.redis_cache.dsn,
+    include=["src.adapters.celery.tasks"],
+)
+
+celery.conf.update(
+    timezone="Europe/Moscow",
+    enable_utc=True,
+    task_serializer="json",
+    result_serializer="json",
+    accept_content=["json"],
+)
